54ebf27f7bce48f93c34b6624fd52530c8d44a15
Daniel Barnes <danielbarnes175@gmail.com>
2024-03-12 18:59:49 -0500

    Add Tiled boilerplate

---
+++ GateAdventures/Engine/TiledMapLoader.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GateAdventures.Engine
+{
+	public class TiledMapLoader
+	{
+		public TiledMapLoader()
+		{
+			
+		}
+	}
+}
--- GateAdventures/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿
-using var game = new GateAdventures.Game1();
-game.Run();
